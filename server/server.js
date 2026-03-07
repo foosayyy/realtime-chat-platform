9f443736070539ae5343564ddef7d33b6c27e459
Krishan Rai <dankrish26@gmail.com>
2026-03-07 23:59:10 +0530

    added online users list and typing indicator

--- server/server.js
+++ server/server.js
@@ -12,27 +12,61 @@ const io = new Server(server, {
   cors: { origin: "*" },
 });
 
+const rooms = {};
+
 io.on("connection", (socket) => {
   console.log("User connected");
 
   socket.on("join_room", (data) => {
     socket.join(data.room);
 
-    socket.to(data.room).emit("receive_message", {
+    socket.username = data.username;
+    socket.room = data.room;
+
+    if (!rooms[data.room]) {
+      rooms[data.room] = [];
+    }
+
+    rooms[data.room].push({
+      username: data.username,
+      avatar: data.avatar,
+    });
+
+    io.to(data.room).emit("receive_message", {
       id: Date.now(),
       username: "System",
       avatar: "⚡",
-      message: data.username + " joined the room",
+      message: `${data.username} joined the room`,
     });
+
+    io.to(data.room).emit("room_users", rooms[data.room]);
   });
 
   socket.on("send_message", (data) => {
     io.to(data.room).emit("receive_message", data);
   });
-});
 
-socket.on("typing", (data) => {
-  socket.to(data.room).emit("user_typing", data.username);
+  socket.on("typing", (data) => {
+    socket.to(data.room).emit("user_typing", data.username);
+  });
+
+  socket.on("disconnect", () => {
+    const room = socket.room;
+    const username = socket.username;
+
+    if (room && rooms[room]) {
+      rooms[room] = rooms[room].filter((user) => user.username !== username);
+
+      io.to(room).emit("room_users", rooms[room]);
+
+      io.to(room).emit("receive_message", {
+        id: Date.now(),
+        username: "System",
+        avatar: "⚡",
+        message: `${username} left the room`,
+      });
+    }
+  });
 });
 
 server.listen(5000, () => {
